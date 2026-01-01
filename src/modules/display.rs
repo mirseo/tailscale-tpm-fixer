003ddf41c44717960aa94cb143dd78f42af84a8c
mirseo <starlights2283@gmail.com>
2026-01-02 00:01:24 +0900

    feat: add os detect and remove file message

--- src/modules/display.rs
+++ src/modules/display.rs
@@ -6,15 +6,27 @@ pub fn show_logo() {
   | |   | | |  _|
   |_|   |_| |_|
 
-  tailscale-tpm-fixer 
+  tailscale-tpm-fixer
 
-  This tool deletes Tailscale files associated with the TPM from your system.  
+  This tool deletes Tailscale files associated with the TPM from your system.
 
   After the process is complete, please re-register your device to reactivate the machine.
 
   Specifically, the following files will be removed.
+"#);
 
+    #[cfg(target_os = "windows")]
+    println!(r#"
+    - C:\ProgramData\Tailscale
+    - C:\Users\%USERNAME%\AppData\Local\Tailscale
+"#);
 
+    #[cfg(target_os = "linux")]
+    println!(r#"
+    - /var/lib/tailscale/tailscaled.state
+"#);
+
+    println!(r#"
   Press Y to agree and continue, or N to cancel.
 
   "#);
